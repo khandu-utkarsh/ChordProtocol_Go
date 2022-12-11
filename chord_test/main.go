e1be2272d1610cfcaf18bd546d084e1d682fb7c6
utkarsh <utkarshkhandelwal2011@gmail.com>
2022-12-11 03:51:26 -0500

    Created two separate packages

--- chord_test/main.go
+++ chord_test/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/khandu-utkarsh/ChordProtocol_Go"
+	"ChordProtocol_Go/chord"
 	"fmt"
 )
 
@@ -9,27 +9,26 @@ func main() {
 
 	fmt.Print("Utkarsh's chord-go code\n")
 
-	curr_node_ip := GetCurrentProcessIPAddress()
-	curr_node_port := GetCurrentProcessPort()
+	curr_node_ip := chord.GetCurrentProcessIPAddress()
+	curr_node_port := chord.GetCurrentProcessPort()
 
 	stringToBeHashed := curr_node_ip + curr_node_port
 
-	var curr_ip_port_hash HashId
-	curr_ip_port_hash = Generate_Hash([]byte(stringToBeHashed))
+	var curr_ip_port_hash chord.HashId
+	curr_ip_port_hash = chord.Generate_Hash([]byte(stringToBeHashed))
 
 	//!Initializing node data
-	var curr_node Node
-
-	curr_node.node_id = curr_ip_port_hash
-	curr_node.node_ip_address = curr_node_ip
-	curr_node.node_port_number = curr_node_port
+	var curr_node chord.Node
+	curr_node.Set_node_id(curr_ip_port_hash)
+	curr_node.Set_node_ip_address(curr_node_ip)
+	curr_node.Set_node_port_number(curr_node_port)
 
 	processNode := curr_node.Create()
 
-	var keyToFind HashId
-	nodeFound := processNode.lookup(keyToFind)
-	fmt.Print("IP Adress: ", nodeFound.node_ip_address, "\n")
-	fmt.Print("Port Number: ", nodeFound.node_port_number, "\n")
+	var keyToFind chord.HashId
+	nodeFound := processNode.Lookup(keyToFind)
+	fmt.Print("IP Adress: ", nodeFound.Get_node_ip_address(), "\n")
+	fmt.Print("Port Number: ", nodeFound.Get_node_port_number(), "\n")
 
 	return
 }
