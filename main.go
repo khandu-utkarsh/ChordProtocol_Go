3f8808efbd768d49fe1a141e28acbbae9c7f4991
neerajkanuri <neerajkanuri@gmail.com>
2022-12-12 16:10:33 -0500

    adding chat code

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/khandu-utkarsh/ChordProtocol_Go/chat"
 	"github.com/khandu-utkarsh/ChordProtocol_Go/chord"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func main() {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+	chat.RegisterChatServiceServer(grpcServer, &s)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 
 	fmt.Print("Utkarsh's chord-go code\n")
 
